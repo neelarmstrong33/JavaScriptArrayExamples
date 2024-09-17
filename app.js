f221a736320bb8db7731a85ee697c355dfc4f3f3
Neel <neelarmstrong33@gmail.com>
2024-09-17 16:12:03 +0530

    Code upto Splice Method uses in Array.

---
+++ app.js
@@ -0,0 +1,56 @@
+/*
+const numbers = [1,2,3];
+console.log(numbers);
+
+const moreNumbers = new Array('Hi!', 'Welcome');
+console.log(moreNumbers);
+
+const another = Array(5);
+console.log(another);
+
+const yetMoreNumbers = Array.of(1,2);
+console.log(yetMoreNumbers);
+
+const someMoreNumber = Array.from('Hi');
+console.log(someMoreNumber);
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);
+
+const hobbies = ['Cooking', 'Reading'];
+const personData = [30, 'Neel', { moreDetails: []}];
+
+const analyticsData = [[1,1.6], [-5.4,2.1]];
+for(const data of analyticsData) {
+    for(const dataPoint of data){
+        console.log(dataPoint);
+    }
+}
+*/
+
+/*
+const hobbies = ['Reading Books', 'Sports'];
+hobbies.push('singing'); 
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+hobbies[5] = 'Cooking';
+console.log(hobbies,hobbies[4]);
+*/
+
+/*
+const hobbies = ['Cooking', 'Singing'];
+hobbies.splice(0, 0, 'Coding');
+hobbies.splice(1, 0, 'Reading');
+hobbies.splice(0, 1);
+hobbies.splice(0); //will delete everything in the array
+const removedElements = hobbies.splice(-1,1);
+console.log(hobbies);
+*/
+
